973569243e787b728ec71366131eb75da34556ee
InuHone <noah.stoeckel@gmail.com>
2024-03-31 17:32:09 -0400

    set up my own project

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export WANDB_PROJECT=heron
-export PROJECT_NAME=opt/exp001
+export WANDB_PROJECT=bbheron
+export PROJECT_NAME=my_vlm/exp001
 export WANDB_NAME=$PROJECT_NAME
 
 deepspeed train.py --config_file projects/$PROJECT_NAME.yml
